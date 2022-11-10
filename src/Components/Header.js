ba64b91fed5b07758df0707cb2d0b36c95b8968a
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-11-09 21:12:23 -0500

    fixing the header and adding donation tabs

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,24 +9,57 @@ const Header = () => {
     setLocation(window.location.href.split("/")[3]);
   }, []);
 
+  const transform = {
+    animation: "rotate 1s infinite",
+    animationDuration: "15s",
+  };
+
   //the setlocation below is recieving the different data locations everytime it changes tabs
   return (
     <>
-      <div className="px-4 py-4 header-bg-color">
-        <div className="flex justify-around mx-auto items-center relative text-xl">
-          <a href="/" className="inline-flex">
-            <img
-              src="https://yt3.ggpht.com/ytc/AMLnZu-V4A__CO2hzwdI2FSdLZylV0BxZcB_ahuPlNPsQw=s88-c-k-c0x00ffffff-no-rj"
-              alt="eroforce's icon"
-              className="object-contain rounded-full border-2 border-gray-800 bg-gray-900 p-1 w-16 h-16"
-              title="DarkSoul Elitest"
-            ></img>
-            <span className="ml-3 my-auto wg-heading-color font-bold">
-              EroForce Tank Station
-            </span>
-          </a>
-          {location === "" ? <div className=""></div> : <div>searchbar</div>}
-          <div className="">03</div>
+      <div className="px-4 filter relative z-50 py-4 header-bg-color">
+        <div class="px-4 py-4 mx-auto md:max-w-full lg:px-6 lg:max-w-screen-2xl">
+          <div className="relative flex items-center justify-between">
+            <a href="/" className="inline-flex">
+              <img
+                src="https://yt3.ggpht.com/ytc/AMLnZu-V4A__CO2hzwdI2FSdLZylV0BxZcB_ahuPlNPsQw=s88-c-k-c0x00ffffff-no-rj"
+                alt="eroforce's icon"
+                className="object-contain rounded-full border-2 border-gray-800 bg-gray-900 p-1 w-16 h-16"
+                title="DarkSoul Elitest"
+              ></img>
+              <span className="ml-3 my-auto wg-heading-color font-bold">
+                EroForce Tank Station
+              </span>
+            </a>
+            {location === "" ? (
+              <div className=""></div>
+            ) : (
+              <div className="w-3/5 md:w-2/5 relative">
+                <input
+                  className="bg-wg-mutted placeholder-wg-page text-wg-page px-4 py-2 rounded-md w-full focus:bg-wg-heading focus:ring-wg-accent focus:border-wg-accent"
+                  type="search"
+                  name="vehicle"
+                  placeholder="Vehicle name (min 2 characters) in 543 vehicles"
+                  autoComplete="off"
+                  value=""
+                ></input>
+              </div>
+            )}
+            <div className="flex">
+              <a
+                href="https://www.youtube.com/c/ReplaysArchive"
+                target="_replaysarchive"
+                title="Daily tier 8+ World of Tanks replays curated by ParadoxLector"
+              >
+                <img
+                  className="w-20 mr-6 hidden xl:inline-block"
+                  src={require("../Images/Status/replays.PNG")}
+                  alt="Daily tier 8+ World of Tanks replays curated by ParadoxLector"
+                  style={transform}
+                ></img>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
 
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -25,6 +25,11 @@ const Nav = ({ setLocation }) => {
     } else return "Contact";
   };
 
+  const bmc = {
+    fontSize: "14px",
+    fontWeight: "600",
+  };
+
   //creates an array to pick from easier
   let path = window.location.href.split("/");
 
@@ -49,6 +54,40 @@ const Nav = ({ setLocation }) => {
             </Link>
           </ul>
         ))}
+        <div className="flex px-8">
+          <a
+            className="text-wg-mutted transition-colors duration-300 hover:text-wg-accent w-8"
+            href="https://www.patreon.com/skill4ltu"
+            rel="noreferrer"
+            target="_blank"
+            aria-label="Patreon"
+            title="skill4ltu Patreon"
+          >
+            <svg
+              className="h6"
+              fill="#ff424d"
+              aria-aria-hidden="true"
+              role="img"
+              preserveAspectRatio="xMidYMid meet"
+              viewBox="0 0 24 24"
+            >
+              <path d="M13.464 7.206c-1.976 0-3.583 1.598-3.583 3.563c0 1.958 1.607 3.551 3.583 3.551c1.969 0 3.57-1.593 3.57-3.551a3.57 3.57 0 0 0-3.57-3.563m-6.38 9.49v-9.49h1.752v9.49z"></path>
+              <path d="M6 4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H6zm0-2h12a4 4 0 0 1 4 4v12a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V6a4 4 0 0 1 4-4z"></path>
+            </svg>
+          </a>
+          <a
+            className="bmc-button my-auto ml-1"
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.buymeacoffee.com/skill4ltuindex"
+            title="Donate a floof"
+          >
+            <span>
+              🦊
+              <em style={bmc}>Donate a floof</em>
+            </span>
+          </a>
+        </div>
       </nav>
     </div>
   );
